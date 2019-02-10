2fec2f094606d0e17aac250fa2df765b7de27d8f
Kyle Milloy <kmilloy@nubix.ca>
2019-02-09 21:58:48 -0700

    init tests and travis

---
+++ tests/NowCal/NowCalTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\NowCal;
+
+use Tests\TestCase;
+
+class NowCalTest extends TestCase
+{
+    /** @test */
+    public function it_has_a_test()
+    {
+        $this->assertTrue(true);
+    }
+}
